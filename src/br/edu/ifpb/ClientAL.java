c2396131e2cf3b2f18aa6eed0cb24fb10f1a64b2
Joffily Ferreira <joffily@outlook.com>
2017-06-21 21:53:19 -0300

    ApacheLittle Cliente refatorado :rocket:
    Este commit refatora o código do cliente para que os parâmetros de entrada possam ser passados via argumentos na linha de comando.
    Também foi adicionado um regex pattern para extrair as informações de entrada (ip:porta/file).

--- src/br/edu/ifpb/ClientAL.java
+++ src/br/edu/ifpb/ClientAL.java
@@ -2,29 +2,54 @@ package br.edu.ifpb;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ClientAL {
+	private static Pattern pattern = Pattern.compile("(\\d+.\\d+.\\d+.\\d+|\\w+):(\\d+)(.*)");
+
 	public static void main(String[] args) {
-		try {
-			Scanner sc = new Scanner(System.in);
-		
-			do {
-				System.out.println("Informe o nome do arquivo:");
-				Socket serverSocket = new Socket("localhost",5600);
-				DataOutputStream dataOutput = new DataOutputStream(serverSocket.getOutputStream());
-				DataInputStream dataInput = new DataInputStream(serverSocket.getInputStream());
-				
-				dataOutput.writeUTF(sc.nextLine());
-				String response = dataInput.readUTF();
-				System.out.println("Response: "+response);
-				System.out.println(".........................................................");
-				serverSocket.close();
-			} while (true);
-			
-		} catch (Exception e) {
-			// TODO: handle exception
+
+		System.out.println("Bem vindo ao cliente do Apache Little");
+
+		Matcher matcher;
+
+		if (args.length == 0) {
+			System.out.println("Informe um endereço para conectar: ");
+			Scanner teclado = new Scanner(System.in);
+			matcher = pattern.matcher(teclado.nextLine());
+		} else {
+			matcher = pattern.matcher(args[0]);
 		}
+
+		if (!matcher.find()) {
+			System.out.println("Entrada incorreta, experimente algo como: 127.0.0.1:6500/index.html");
+			return;
+		}
+
+		// Conecta ao servidor
+		try {
+			Socket socket = new Socket(matcher.group(1), Integer.parseInt(matcher.group(2)));
+
+			if (!socket.isConnected()) {
+				System.out.println("Não foi possível conectar ao servidor");
+				return;
+			}
+
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+
+			// Requisita arquivo
+			output.writeUTF(matcher.group(3));
+
+			// Escreve resposta
+			System.out.println(input.readUTF());
+		} catch (IOException e) {
+			System.out.println("Não foi possível realizar operação ao servidor especificado");
+		}
+
 	}
 }
