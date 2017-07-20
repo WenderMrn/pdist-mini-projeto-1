18ee661fac2d49c816310aeaa5f759b386c13fbb
WenderMrn <wender.mrn@gmail.com>
2017-07-20 06:24:53 -0300

    acaba mais não

--- src/br/edu/ifpb/entity/ApacheLite.java
+++ src/br/edu/ifpb/entity/ApacheLite.java
@@ -1,8 +1,8 @@
 package br.edu.ifpb.entity;
 
-import java.net.MalformedURLException;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.nio.channels.AlreadyBoundException;
-import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -19,9 +19,10 @@ import br.edu.ifpb.utils.FileHelper;
 public class ApacheLite extends UnicastRemoteObject implements IApacheManager, IApacheClient{
 	
 	private static final long serialVersionUID = 1L;
+	private static int port = 1078;
 	private HashMap<String, String> managers;
 	private ArrayList<IManagerRemote> loggedManagers;
-	private static String acessoNegado = "\nAcesso negado! Faça login novamente.\n";
+	private static String acessoNegado = "\nAcesso negado! Faça login.\n";
 	
 	public ApacheLite()throws RemoteException{
 		this.managers = new HashMap<String, String>();
@@ -32,10 +33,16 @@ public class ApacheLite extends UnicastRemoteObject implements IApacheManager, I
 	public static void main(String[] args){
 		try {
 			
-			Registry registry = LocateRegistry.createRegistry(1078);
+			Registry registry = LocateRegistry.createRegistry(port);
 			ApacheLite servidor = new ApacheLite();
 			registry.bind("ApacheLiteRMI", servidor);
 			System.out.println("ApacheLite RMI Inicializado..");
+			try {
+				System.out.println("Host: "+Inet4Address.getLocalHost().getHostAddress()+":"+port);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}
 		
 		} catch (RemoteException e) {
 			e.printStackTrace();
@@ -87,8 +94,6 @@ public class ApacheLite extends UnicastRemoteObject implements IApacheManager, I
 		if(FileHelper.createFile(name, content)){
 			//notify
 			for (IManagerRemote iManagerRemote : loggedManagers) {
-				System.out.println("A: "+iManagerRemote.getLogin());
-				System.out.println("B: "+manager.getLogin());
 				if(!iManagerRemote.getLogin().equals(manager.getLogin()))
 					iManagerRemote.notify("O usuário "+manager.getLogin()+" criou o arquivo "+name+"!");
 			}
@@ -105,8 +110,6 @@ public class ApacheLite extends UnicastRemoteObject implements IApacheManager, I
 		if(FileHelper.deleteFile(name)){
 			//notify
 			for (IManagerRemote iManagerRemote : loggedManagers) {
-				System.out.println("A: "+iManagerRemote.getLogin());
-				System.out.println("B: "+manager.getLogin());
 				if(!iManagerRemote.getLogin().equals(manager.getLogin()))
 					iManagerRemote.notify("O usuário "+manager.getLogin()+" deletou o arquivo "+name+"!");
 			}
@@ -134,7 +137,9 @@ public class ApacheLite extends UnicastRemoteObject implements IApacheManager, I
 		
 		if(manager == null ) return mr;
 		
-		if(this.managers.get(manager.getLogin()).equals(manager.getPassword())){
+		if (this.managers.get(manager.getLogin()) != null &&
+				(this.managers.get(manager.getLogin()).equals(manager.getPassword()))) {
+
 			if(!isLogged(manager.getLogin())){
 				this.loggedManagers.add(manager);
 				System.out.println(this.loggedManagers.size());
