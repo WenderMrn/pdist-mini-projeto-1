869cd96c662a25a0cb430d94f68cc5ae836c5c1e
Wender Mrn <wender.mrn@gmail.com>
2017-07-20 01:15:09 -0300

    ajustando a class clientAl

--- src/br/edu/ifpb/entity/ClientAL.java
+++ src/br/edu/ifpb/entity/ClientAL.java
@@ -4,8 +4,10 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 import br.edu.ifpb.interfaces.IApacheClient;
+import br.edu.ifpb.utils.ApacheLiteException;
 
 public class ClientAL {
 	
@@ -13,11 +15,25 @@ public class ClientAL {
 
 		System.out.println("Bem vindo ao cliente do Apache Lite RMI");
 
+		Scanner teclado = new Scanner(System.in);
+		String arquivo = "";
+		
 		try {
 			
 			IApacheClient servidor = (IApacheClient) Naming.lookup("rmi://localhost/ApacheLiteRMI");
-			System.out.println(servidor.searchFile("doc.txt"));
 			
+			do{
+				System.out.println("\n\n---------------------------------------------");
+				System.out.println("| ApacheLite 2.0 RMI (para sair digite 0 )  |");
+				System.out.println("|-------------------------------------------|\n\n");
+				System.out.println(" Informe o nome do arquivo: ");
+				
+				arquivo = teclado.nextLine();
+		
+				System.out.println(servidor.searchFile(arquivo));
+				
+			}while(!arquivo.equals("0"));
+		
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
